68c20630f4246502911671651a56b5ff5b7b2734
Alex Hartford <hartforda@msoe.edu>
2018-05-14 22:20:15 -0500

    demo

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,27 @@
 public class Driver {
 
     public static void main(String[] args) {
-        Article a1 = new Article("Test");
-        Article a2 = new Article("Test2", a1);
-        Article a3 = new Article("Test3");
-        Article a4 = new Article("Test4", a2, a3);
+        PageRank.rank(generateArticleData());
+    }
 
-        PageRank.rank(a1, a2, a3, a4);
+    /**
+     * Highest-Rank Article should be index 0: https://arxiv.org/abs/1604.07316
+     * @return array of articles based on actual articles on google scholar
+     */
+    private static Article[] generateArticleData() {
+        Article[] articles = new Article[10];
+
+        articles[0] = new Article("End to end learning for self-driving cars");
+        articles[1] = new Article("Eyeriss...", articles[0]);
+        articles[2] = new Article("Towards evaluating the robustness of neural networks", articles[0]);
+        articles[3] = new Article("Deep predictive coding networks...", articles[0]);
+        articles[4] = new Article("End-to-end learning of driving models...", articles[0]);
+
+        articles[5] = new Article("Backpropagation for energy-efficient neuromorphic computing", articles[1]);
+        articles[6] = new Article("Minerva...", articles[1]);
+        articles[7] = new Article("Cnvlutin", articles[1]);
+
+        
+        return articles;
     }
 }
